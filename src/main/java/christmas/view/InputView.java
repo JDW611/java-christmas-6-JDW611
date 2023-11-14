bd40568a019f2323330398099a521620d5365591
다운 <j_d_woon@naver.com>
2023-11-14 11:19:06 +0900

    feat: 방문날짜 validate - 1~31의 숫자만 입력 가능

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -5,9 +5,9 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     public int readDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        int input = convertToInt(Console.readLine());
-
-        return input;
+        int visitDate = convertToInt(Console.readLine());
+        validateVisitDate(visitDate);
+        return visitDate;
     }
 
     public String readMenu() {
@@ -18,5 +18,10 @@ public class InputView {
     }
     private int convertToInt(String input) {
         return Integer.parseInt(input);
+    }
+    private void validateVisitDate(int visitDate) {
+        if(!(visitDate >= 1 && visitDate <= 31)) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
     }
 }

a8e50bbeeb53c07bdce8ee523717e18a14837e26
다운 <j_d_woon@naver.com>
2023-11-14 23:41:57 +0900

    feat: 방문날짜 추가

--- src/main/java/christmas/models/Order.java
+++ src/main/java/christmas/models/Order.java
@@ -1,10 +1,13 @@
-package christmas.domain;
+package christmas.models;
 
+import christmas.enums.Menu;
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Order {
     private final Map<Menu, Integer> items;
+    private LocalDate visitDate;
 
     public Order() {
         this.items = new HashMap<>();
@@ -12,6 +15,14 @@ public class Order {
 
     public void addItem(Menu item, int quantity) {
         items.put(item, quantity);
+    }
+
+    public LocalDate getVisitDate() {
+        return visitDate;
+    }
+
+    public void setVisitDate(int customerVisitDate) {
+        this.visitDate = LocalDate.of(2023, 12, customerVisitDate);
     }
 
     public Map<Menu, Integer> getItems() {

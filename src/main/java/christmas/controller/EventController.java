cfed0576ffb1163b934a0ff3b230f30709b33ea4
다운 <j_d_woon@naver.com>
2023-11-15 13:54:47 +0900

    refactor: 역활에 따른 메소드 분리

--- src/main/java/christmas/controller/EventController.java
+++ src/main/java/christmas/controller/EventController.java
@@ -7,20 +7,50 @@ import christmas.service.EventService;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 import java.util.List;
+import org.mockito.internal.matchers.Or;
 
 public class EventController {
     private final OutputView outputView = new OutputView();
     private final InputView inputView = new InputView();
     private final EventService eventService = new EventService();
     private final EventDiscountService eventDiscountService = new EventDiscountService();
+
     public void run() {
         outputView.printWelcomeMessage();
-        int visitDate = inputView.readDate();
-        Order orderDetails = eventService.convertToMenuItems(inputView.readMenu(), visitDate);
-        outputView.printEventPreviewForDate(visitDate);
+        Order orderDetails = askToCustomer();
 
         outputView.printOrderMenuDetail(orderDetails);
 
+        previewEvent(orderDetails);
+    }
+
+    private Order askToCustomer() {
+        int visitDate = askVisitDate();
+        return askOrder(visitDate);
+    }
+
+    private int askVisitDate() {
+        try {
+            return inputView.readDate();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return askVisitDate();
+        }
+    }
+
+    private Order askOrder(int visitDate) {
+        try {
+            Order orderDetails = eventService.convertToMenuItems(inputView.readMenu(), visitDate);
+            outputView.printEventPreviewForDate(visitDate);
+            return orderDetails;
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return askOrder(visitDate);
+        }
+    }
+
+    private void previewEvent(Order orderDetails) {
         int orderTotalPrice = eventService.calculateOrderTotalPrice(orderDetails);
         outputView.printOrderTotalPrice(orderTotalPrice);
 
@@ -33,12 +63,12 @@ public class EventController {
         int totalDiscountPrice = eventDiscountService.caculateTotalDiscount(orderDetails, orderTotalPrice);
         outputView.printTotalDiscountPrice(totalDiscountPrice);
 
-
         int expectedPayPrice = eventDiscountService.caculateExpectedPayPrice(orderDetails, orderTotalPrice);
         outputView.printExpectedPayPrice(expectedPayPrice);
 
         String eventBadge = eventService.checkBadgesType(totalDiscountPrice);
         outputView.printEventBadge(eventBadge);
     }
+
 
 }
